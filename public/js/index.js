3a000c253e9d864a7cd852022653719d7e8dacab
arreche <arreche@gmail.com>
2016-08-25 08:52:02 +0100

    Display some products from the static json

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,32 @@
-function displayMessage() {
-  return 'Hello, World!';
+function buildProduct(product) {
+  var div = document.createElement("div");
+  div.className = "product";
+
+  var title = document.createElement("span");
+  title.textContent = product.title;
+  div.appendChild(title);
+
+  var picture = document.createElement("img");
+  picture.src = product.picture;
+  div.appendChild(picture);
+
+  var price = document.createElement("span");
+  price.textContent = product.price;
+  div.appendChild(price);
+
+  return div;
 }
 
-console.log(displayMessage());
+function buildProducts(products) {
+  var div = document.createElement("div");
+  div.className = "products";
 
-console.log(products);
+  for (var i = 0; i < products.length; i++) {
+    div.appendChild(buildProduct(products[i]));
+  }
+
+  return div;
+}
+
+var app = document.getElementById("app");
+app.appendChild(buildProducts(products));
--- public/js/products.js
+++ public/js/products.js
@@ -1,4 +1,34 @@
 var products = [
-  {id:1},
-  {id:2}
+  {
+    id: 1,
+    title: "Duff",
+    picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkaOwLSA8zTMSancM1OKuXlm7nylciCxyyLxsUUomjZ6WrGahScQ",
+    price: "1€"
+  },
+  {
+    id: 2,
+    title: "Heineken",
+    picture: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQNdBfmjsAggKzg6ecQYgijQwRFiw15v905j2B3Wj8ATFHaIpTNgQ",
+    price: "2€"
+  },
+  {
+    id: 3,
+    title: "San Miguel",
+    picture: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcThBWOSSsqwKYl-TGun4RwrqNcsoyEV6V_Ku7r4c8QScg-ZZeZscQ",
+    price: "1.5€"
+  },
+  {
+    id: 4,
+    title: "Guiness",
+    picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3CTtq7eEDpfTf1vqjd_SbZoQSq66pv7QjUAdSkaW_P5U60D8cCg",
+    price: "3€"
+  },
+  {
+    id: 4,
+    title: "Budweiser",
+    picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGpbAA7RgQRppViAOyNfG0VtOD9us0BoU0EaacRc31hoH4lB4Q",
+    price: "1.99€"
+  },
+
+
 ]
